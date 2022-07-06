b1243f3f0fa692b90608f70779e87d2fccf581f7
Ryan Darcey <rtdarcey@wpi.edu>
2022-07-06 17:29:22 -0400

    added stats script

---
+++ CompileStats.py
@@ -0,0 +1,14 @@
+import csv
+
+# global vars (TODO: probably change to args)
+target_csv_1 = ""
+
+
+# calculates the total average Db level for every hour
+# -> each different on the clock, not every 60 minutes/3600 seconds
+def average_db_per_hour():
+    x = 0
+
+
+if __name__ == '__main__':
+    average_db_per_hour()
--- TrimmingCSV.py
+++ TrimmingCSV.py
@@ -1,7 +1,14 @@
 import csv
 
+# global vars (TODO: probably change to args)
+target_csv_1 = ""
+target_csv_2 = ""   # idk if this is necessary (would be RTA file)
+
+# probably a list of columns or something idk
+
 
 def trim_csv():
+    # not even sure we really need this necessarily, we'll figure it out later
     x = 0
 
 
